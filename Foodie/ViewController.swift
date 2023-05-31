a9caa3e0cac7c417d926c05a3a6a3bf4c4478a61
Rinki Etheridge <retheridge@hyperice.com>
2023-05-30 19:22:10 -0500

    Initial Commit

---
+++ Foodie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodie
+//
+//  Created by Rinki Etheridge on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
